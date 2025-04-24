a5a44ca4c45047086758d46be08cd4f68c9166a8
Jun2728 <legalmind94@daum.net>
2025-04-24 17:29:39 +0900

    Delete Java Sql directory

--- Java Sql/0424/042402 Q32.sql
+++
@@ -1,9 +0,0 @@
-use my_cat;
-
-show tables;
-select * from board;
-
-insert into board (b_title,b_id,b_datetime,b_txtx,b_hit)
-
-values ('콘솔프로그램에서쓴글', 'kiity', now(), '콘솔프로그램에서 작성한 글입니다. 글글.....글....',0);
-
